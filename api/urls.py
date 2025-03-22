2538ffb1431e5f613c5e983d5367e1b1b6f81c62
Mieda0310 <miedas@colabmix.co.jp>
2025-03-22 15:38:27 +0900

    [new] apiと親のurls.pyのルーティング設定

---
+++ api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+app_name = "user"
+
+## ! routerが使えるのはviewsetsを使っているものだけ
+router = DefaultRouter()
+router.register("profile", views.ProfileViewSet)
+router.register("post", views.PostViewSet)
+router.register("post/comment", views.CommentViewSet)
+
+## ! genericsで定義したものはこっちを使う
+urlpatterns = [
+    path("register/", views.CreateUserView.as_view(), name="register"),
+    path("myprofile/", views.MyProfileListView.as_view(), name="myprofile"),
+    path("", include(router.urls)),
+]
