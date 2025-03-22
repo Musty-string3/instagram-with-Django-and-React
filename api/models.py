713805bcf4a60a32bf16dbb3afe7b9e341fc812a
Mieda0310 <miedas@colabmix.co.jp>
2025-03-22 13:31:22 +0900

    [new] ユーザープロフィールモデルの作成

--- api/models.py
+++ api/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
+
+def upload_avatar_path(instance, filename):
+    ## ! extには拡張子（jpegやpngが入る）
+    ext = filename.split(".")[-1]
+    ## ! avatarsというフォルダに「ユーザープロフィールのID + ユーザーネーム + 拡張子」という名前でプロフィール画像のpath名が格納されていく
+    return "/".join(["avatars", str(instance.userProfile.id) + str(instance.username) + str(".") + str(ext)])
+
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         """通常のユーザーを作成するメソッド"""
@@ -40,6 +48,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+    class Meta:
+        verbose_name_plural = '02-01.ユーザー'
+        db_table = 'user'
 
     def __str__(self):
         return self.email
+
+class Profile(models.Model):
+    username = models.CharField(verbose_name="ユーザーネーム", max_length=50)
+    userProfile = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        verbose_name="ユーザープロフィール",
+        on_delete=models.CASCADE,
+        related_name="userProfile"
+    )
+    img = models.ImageField(verbose_name="プロフィール画像", upload_to=upload_avatar_path, blank=True, null=True)
+    created_at = models.DateTimeField("作成日時", auto_now_add=False)
+    updated_at = models.DateTimeField("変更日時", auto_now=False)
+
+    class Meta:
+        verbose_name_plural = '02-02.ユーザープロフィール'
+        db_table = 'user'
+
+    def __str__(self):
+        return self.username
